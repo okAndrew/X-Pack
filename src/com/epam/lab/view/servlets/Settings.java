fdcf326c511b2e8bef9695d325bed35262730d37
savruksergiy <savruksergiy@gmail.com>
2013-10-11 00:13:23 +0300

    edited menu, settings

--- src/com/epam/lab/view/servlets/Settings.java
+++ src/com/epam/lab/view/servlets/Settings.java
@@ -9,6 +9,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.epam.lab.controller.services.UserService;
+import com.epam.lab.model.User;
+
 @WebServlet("/settings")
 public class Settings extends HttpServlet {
 
@@ -16,11 +19,10 @@ public class Settings extends HttpServlet {
 	private static final String SETTINGS_JSP = "WEB-INF/jsp/user/settings.jsp";
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher requestDispatcher = request.getRequestDispatcher(SETTINGS_JSP);
-		requestDispatcher.forward(request, response);
-	}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		UserService userService = new UserService();
+		Long id = (Long) (request.getSession().getAttribute("userid"));
+		User user = userService.getUserById(id);
+		request.setAttribute("user", user);
 		RequestDispatcher requestDispatcher = request.getRequestDispatcher(SETTINGS_JSP);
 		requestDispatcher.forward(request, response);
 	}

863a15e6b693672be13cea0524850ff79b009281
Andrew <andrew.kravchuk01@gmail.com>
2013-10-16 15:11:30 +0300

    Add user filters

---
+++ src/com/epam/lab/controller/web/filters/UserVertifyFilter.java
@@ -0,0 +1,49 @@
+package com.epam.lab.controller.web.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet Filter implementation class UserVertifyFilter
+ */
+@WebFilter(urlPatterns = {"/search", "/userpage"}) // etc 
+public class UserVertifyFilter implements Filter {
+
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		HttpSession session = httpRequest.getSession(false);
+		if (session != null) {
+			Object userid = session.getAttribute("userid");
+			if (userid != null) {
+				chain.doFilter(request, response);
+				return;
+			}
+		} else {
+			HttpServletResponse httpResponse = (HttpServletResponse) response;
+			httpResponse.sendRedirect("/signin");
+		}
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+
+	}
+}

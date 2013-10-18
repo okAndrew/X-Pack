6faede128d9ee84d38d3a5e4ac6f866763fba5ac
Andrew <andrew.kravchuk01@gmail.com>
2013-10-18 18:59:11 +0300

    Refactoring ResultSetTransformer.

--- src/com/epam/lab/controller/dao/querymanaging/rs/ResultSetTransformer.java
+++ src/com/epam/lab/controller/dao/querymanaging/rs/ResultSetTransformer.java
@@ -5,47 +5,66 @@ import java.lang.reflect.Field;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import org.apache.log4j.Logger;
+
 import com.epam.lab.controller.annotations.TableColumn;
+import com.epam.lab.controller.exceptions.NoSuchDAOObjectException;
 
 public class ResultSetTransformer<T> {
+	private Logger logger = Logger.getLogger(this.getClass());
 	private Class<?> type;
 
 	public ResultSetTransformer() {
+		super();
 	}
 
 	public ResultSetTransformer(Class<?> cls) {
 		this.type = cls;
 	}
 
-	// rewrite this
-	@SuppressWarnings("unchecked")
-	public T getObject(ResultSet rs) throws SQLException {
+	public T createObject(ResultSet rs) throws SQLException,
+			NoSuchDAOObjectException {
 		// reflection magic
 		Field[] fields = type.getDeclaredFields();
-		T result = null;
-		try {
-			result = (T) type.newInstance();
-		} catch (InstantiationException | IllegalAccessException e1) {
-			e1.printStackTrace();
-		}
+		T result = createInstance();
 		for (Field field : fields) {
 			Annotation[] anns = field.getDeclaredAnnotations();
-			if (anns.length < 1) {
-				continue;
-			}
-			if (anns[0] instanceof TableColumn) {
-				TableColumn tColumn = (TableColumn) anns[0];
+			TableColumn tColumn = findTableColumnAnn(anns);
+			if (tColumn != null) {
 				String colName = tColumn.value();
 				Object atr = rs.getObject(colName);
 				field.setAccessible(true);
 				try {
 					field.set(result, atr);
 				} catch (IllegalArgumentException | IllegalAccessException e) {
-					e.printStackTrace();
+					logger.error(e);
 				}
 			}
 
 		}
 		return result;
+	}
+
+	@SuppressWarnings("unchecked")
+	private T createInstance() throws NoSuchDAOObjectException {
+		T result = null;
+		try {
+			result = (T) type.newInstance();
+		} catch (InstantiationException | IllegalAccessException e) {
+			logger.error(e);
+			throw new NoSuchDAOObjectException("Error constructor execute");
+		}
+		return result;
+	}
+
+	public TableColumn findTableColumnAnn(Annotation anns[]) {
+		TableColumn resultAnn = null;
+		for (Annotation a : anns) {
+			if (a instanceof TableColumn) {
+				resultAnn = (TableColumn) a;
+				break;
+			}
+		}
+		return resultAnn;
 	}
 }

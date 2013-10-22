13e1ef911ac613785dddb8140ea44586627cdfbb
Andrew <andrew.kravchuk01@gmail.com>
2013-10-22 11:53:57 +0300

    Edit Token4Upload DAO

--- src/com/epam/lab/controller/dao/token4upload/Token4UploadDAO.java
+++ src/com/epam/lab/controller/dao/token4upload/Token4UploadDAO.java
@@ -4,5 +4,7 @@ import com.epam.lab.controller.dao.GenericDAO;
 import com.epam.lab.model.Token4Upload;
 
 public interface Token4UploadDAO extends GenericDAO<Token4Upload> {
+	public Token4Upload getByToken(String token);
 
+	public int deleteNotActiveTokens();
 }

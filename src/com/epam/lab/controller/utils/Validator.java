02ec2c076d5049dc2c88d31fb76bc8c42ed08343
Andrew <andrew.kravchuk01@gmail.com>
2013-11-04 01:26:41 +0200

    Add German language to file-folder validator

--- src/com/epam/lab/controller/utils/Validator.java
+++ src/com/epam/lab/controller/utils/Validator.java
@@ -4,32 +4,30 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public enum Validator {
-	
-	USER_LOGIN("[A-Za-z0-9]{4,16}$"),
-	USER_EMAIL("^([0-9a-zA-Z]+[-._+&amp;])*[0-9a-zA-Z]+@([-0-9a-zA-Z]+[.])+[a-zA-Z]{2,6}$"),
-	USER_PASSWORD("^[0-9A-Za-z]{1,32}$"),
-	MD5_CHECKSUM("[a-fA-F0-9]{32}"),
-	TARIFF_NAME("[A-Za-z]{3,15}$"),
-	INTEGERS("[0-9]{9}"),
-	DECIMALS("[0-9]{1,4}.[0-9]{0,2}"),
-	FILE_NAME("[A-Za-z¿-ﬂ‡-ˇ®∏≤≥Øø…È™∫0-9-._\\s\\(\\)]{1,50}$");
-	
+
+	USER_LOGIN("[A-Za-z0-9]{4,16}$"), 
+	USER_EMAIL("^([0-9a-zA-Z]+[-._+&amp;])*[0-9a-zA-Z]+@([-0-9a-zA-Z]+[.])+[a-zA-Z]{2,6}$"), 
+	USER_PASSWORD("^[0-9A-Za-z]{1,32}$"), MD5_CHECKSUM("[a-fA-F0-9]{32}"), 
+	TARIFF_NAME("[A-Za-z]{3,15}$"), INTEGERS("[0-9]{9}"), 
+	DECIMALS("[0-9]{1,4}.[0-9]{0,2}"), 
+	FILE_NAME("[A-Za-z–ê-–Ø–∞-—è–Å—ë–Ü—ñ–á—ó–ô–π–Ñ—î√Ñ√§√ñ√∂√ú√º0-9-._\\s\\(\\)]{1,50}$");
+
 	private String regex;
-	
+
 	private Validator(String regex) {
 		this.regex = regex;
 	}
-	
+
 	public boolean validate(String string) {
 		boolean res = false;
-		
+
 		if (string != null) {
 			Pattern pattern = Pattern.compile(this.regex);
-	        Matcher matcher = pattern.matcher(string);
-	        res = matcher.matches();
+			Matcher matcher = pattern.matcher(string);
+			res = matcher.matches();
 		}
 
-        return res;
+		return res;
 	}
 
 }

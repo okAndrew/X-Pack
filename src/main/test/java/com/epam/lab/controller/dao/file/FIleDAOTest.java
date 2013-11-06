b63f511af820e7e40e2a2ce839dc060e86fd5917
Andrew <andrew.kravchuk01@gmail.com>
2013-11-07 00:16:38 +0200

    Edited jsp tag SizeFormatter. Add junit libs. Create folders for tests

---
+++ src/main/test/java/com/epam/lab/controller/dao/file/FIleDAOTest.java
@@ -0,0 +1,5 @@
+package main.test.java.com.epam.lab.controller.dao.file;
+
+public class FIleDAOTest {
+
+}
---
+++ src/main/test/java/com/epam/lab/controller/services/SignInServiceTest.java
@@ -0,0 +1,7 @@
+package main.test.java.com.epam.lab.controller.services;
+
+import org.junit.Assert;
+
+public class SignInServiceTest extends Assert{
+
+}
